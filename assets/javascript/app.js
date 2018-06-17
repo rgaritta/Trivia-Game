9a2eb43c11eca729173bd42ab0bf3fdab5c4e6e4
rgaritta <fender8@bellsouth.net>
2018-06-16 23:15:33 -0400

    almost there

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,57 +1,92 @@
 var theme = new Audio('assets/audio/theme.mp3');
 var questionNumber = 0;
+var questionsRight = 0;
 
 var game = {
     winAudio: new Audio('assets/audio/win.mp3'),
     loseAudio: new Audio('assets/audio/lose.mp3'),
+    winPic: 'assets/images/win.gif',
+    losePic: 'assets/images/lose.gif',
+
     questions: [
-        'What does Elaine call the passenger she is sitting next to on the plane?',
-        'Who won "The Contest" even though they cheated?',
-        'Which holiday was created by George Costanza?',
-        'What snack does Kramer refer to in his line in the Woody Allen movie?',
-        'What Florida community do the Seinfelds live in?'
-    ],
-
-    answers: [
         {
-            question1: ['Meatball', 'Vegetable Lasagna', 'TV Dinner'],
-            question2: ['George', 'Jerry', 'Kramer'],
-            question3: ['Christmas', 'Hanukkah', 'Festivus'],
-            question4: ['Chips', 'Pretzels', 'Beef Jerky'],
-            question5: ['Boca Raton', 'Pompano Beach', 'Del Boca Vista'],
-        }
+            question: 'What does Elaine call the passenger she is sitting next to on the plane?',
+            answers: ['Meatball', 'Vegetable Lasagna', 'TV Dinner'],
+            image: 'assets/images/question1.png',
+            correct: 'Vegetable Lasagna'
+        },
+        {
+            question: 'Who won "The Contest" even though they cheated?',
+            answers: ['George', 'Jerry', 'Kramer'],
+            image: 'assets/images/question2.jpg',
+            correct: 'George'
+        },
+        {
+            question: 'Which holiday was created by George Costanza?',
+            answers: ['Christmas', 'Hanukkah', 'Festivus'],
+            image: 'assets/images/question3.jpg',
+            correct: 'Festivus'
+
+        },
+        {
+            question: 'What snack does Kramer refer to in his line in the Woody Allen movie?',
+            answers: ['Chips', 'Pretzels', 'Cookies'],
+            image: 'assets/images/question4.jpg',
+            correct: 'Pretzels'
+
+        },
+        {
+            question: 'What Florida community do the Seinfelds live in?',
+            answers: ['Boca Raton', 'Pompano Beach', 'Del Boca Vista'],
+            image: 'assets/images/question5.jpg',
+            correct: 'Del Boca Vista'
+
+        },
     ],
 
-    correctAnswers: ['Vegetable Lasagna', 'George', 'Festivus', 'Pretzels', 'Del Boca Vista'],
+    drawButtons: function () {
 
+        for (var i = 0; i < this.questions[questionNumber].answers.length; i++) {
+            var button = $('<button>');
+            button.addClass('answers');
+            button.text(this.questions[questionNumber].answers[i]);
+            $('#gameplay').append(button);
+            
+        }
+    },
 
+};
 
-
-
-
-
-}
 
 $(".main-pic").on("click", function () {
-    theme.play();
-    $('#question').text('Question ' + (questionNumber + 1) + ': ' + game.questions[questionNumber]);
+    //theme.play();
+    $('#question').text('Question ' + (questionNumber + 1) + ': ' + game.questions[questionNumber].question);
     $('#gameplay').empty();
+    $('.main-pic').attr('src', game.questions[questionNumber].image);
+    $('.main-pic').css('cursor', 'default')
+    game.drawButtons();
+});
 
-})
+$(document).on("click", ".answers", function() {
+    if ($(this).text() == game.questions[questionNumber].correct) {
+        game.winAudio.play();
+        $('.main-pic').attr('src', game.winPic);
+        $('#gameplay').empty();
+        $('#question').text('YOU GOT IT!');
+        questionsRight++;
+    }
+    else {
+        game.loseAudio.play();
+        $('.main-pic').attr('src', game.losePic);
+        $('#gameplay').empty();
+        $('#question').text('The correct answer is: ' + game.questions[questionNumber].correct);
+    }
 
+    $('#gameplay').append($('<h2 id="directions">'));
+    $('#directions').text('Click the picture to answer the next question!');
+    $('.main-pic').css('cursor', 'pointer')
 
+    questionNumber++;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+});
 
